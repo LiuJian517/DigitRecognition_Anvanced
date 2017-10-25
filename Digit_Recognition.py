eca948a1e6f8b7f3348c5e172b781d920f372705
Ljian517 <jianliu517@163.com>
2017-10-25 11:36:52 +0800

    edit files

--- Digit_Recognition.py
+++ Digit_Recognition.py
@@ -1,28 +1,42 @@
-'''
+﻿'''
 @author:LiuJian
-Date:2017-04-01
 '''
 
 #!/usr/bin/python
 #-*- coding:utf-8 -*-
+
 from numpy import *
 import operator
 import csv
 
 def loadTrainData():
+    '''
+    把测试数据读取到列表当中
+    
+    '''
+
     l = []
     with open('train.csv') as file:
         lines = csv.reader(file)
         for line in lines:
             l.append(line)
-
+    
+    # 去掉第一行的描述信息
     l.remove(l[0])
     l = array(l)
+
+    # 提取标签
     label = l[:,0]
+
+    # 提取数据
     data = l[:,1:]
     return nomalizing(toInt(data)),toInt(label)
 
 def toInt(array):
+    '''
+    把数据转成整数类型
+
+    '''
     array = mat(array)
     m,n = shape(array)
     newArray = zeros((m,n))
@@ -32,6 +46,7 @@ def toInt(array):
     return newArray
 
 def nomalizing(array):
+    # 0-1化，数据规整
     m,n = shape(array)
     for i in arange(m):
         for j in arange(n):
@@ -40,6 +55,7 @@ def nomalizing(array):
     return array
 
 def loadTestData():
+    # 读取训练数据并规整
     l = []
     with open('test.csv') as file:
         lines = csv.reader(file)
@@ -50,6 +66,7 @@ def loadTestData():
     return nomalizing(toInt(data))
 
 def classify(inX,dataSet,labels,k):
+    # 将数据转化为矩阵
     inX = mat(inX)
     dataSet = mat(dataSet)
     labels = mat(labels)
@@ -62,6 +79,7 @@ def classify(inX,dataSet,labels,k):
     # print(shape(distances))
     sortedDistIndicies = (distances).argsort()
     classCount = {}
+    # 进行投票
     for i in range(k):
         voteLabel = labels[0,sortedDistIndicies[i]]
         classCount[voteLabel] = classCount.get(voteLabel,0)+1
@@ -69,8 +87,10 @@ def classify(inX,dataSet,labels,k):
     return int(sortedClassCount[0][0])
 
 def saveResult(result):
+    # 保存结果到文件当中
     with open('result3.csv','w',newline='') as myFile:
         myWriter = csv.writer(myFile)
+	    # 写上行头
         myWriter.writerow(["ImageId","Label"])
         cnt = 0
         for i in result:
@@ -89,7 +109,8 @@ def handWritingClassTest():
     errorCount = 0
     resultList = []
     for i in range(m):
-        #classifierResult = classify(testData[i], trainData, trainLabel, 10) memory error
+        # classifierResult = classify(testData[i], trainData, trainLabel, 10) 
+	# memory error
         classifierResult = classify(testData[i],trainData[0:2000],trainLabel[0:2000],5)
         print(classifierResult)
         resultList.append(classifierResult)
@@ -98,7 +119,7 @@ def handWritingClassTest():
 
 
 # 利用scikit-learn包
-def test():
+def pack_test():
     from sklearn import metrics
     from sklearn.neighbors import KNeighborsClassifier
     trainData, trainLabel = loadTrainData()
@@ -109,7 +130,10 @@ def test():
     pridicted = model.predict(testData)
     saveResult(pridicted)
 
-def svm():
+def pack_svm():
+    '''
+    利用scklearn库当中的SVM包，训练支持向量机分类器
+    '''
     from sklearn import svm
     trainData, trainLabel = loadTrainData()
     print(trainData.shape, trainLabel.shape)
@@ -123,7 +147,7 @@ def svm():
 if __name__=='__main__':
     # handWritingClassTest()
     # test()
-    svm()
+    pack_svm()
 
 
 
---
+++ regularExpression.py
@@ -0,0 +1,48 @@
+# coding:utf-8
+import re
+
+'''
+############  正则表达式的主要功能函数：############
+
+函数式用法(一次性)：rst = re.search(r'[1-9]\d{5}','BIT 100081')
+
+面向对象用法：
+pat = re.compile(r'[1-9]\d{5}')
+rst = pat.search('BIT 100081')
+
+'''
+
+match = re.search(r'[1-9]\d{5}',"LiuJian19920517")
+if match:
+    print(match.group(0))
+'''
+如果正则表达式中定义了组，就可以在Match对象上用group()方法提取出子串来。
+
+注意到group(0)永远是原始字符串，group(1)、group(2)……表示第1、2、……个子串。
+'''
+
+##################################################
+
+match = re.match(r'[1-9]\d{5}','100081 BIT')
+if match:
+    print(match.group(0))
+
+##################################################
+
+ls = re.findall(r'[1-9]\d{5}',"LIU19920517 YE19921001")
+print(ls)
+
+#################################################
+result = re.split(r'[1-9]\d{5}',
+                  'BIT100081 TSU100084',
+                  maxsplit = 1)
+print(result)
+
+#####################################
+'''
+贪婪匹配/非贪婪匹配
+
+'''
+match  = re.search(r'PY.*?N','PYANBNCNDN')
+if match:
+    print(match.group(0))
